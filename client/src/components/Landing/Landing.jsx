a37c4d50fa1e0b784a1a6bbb688932285a8975db
Barbara Borini <borini.barbara@gmail.com>
2022-07-12 21:23:21 -0300

    asd

---
+++ client/src/components/Landing/Landing.jsx
@@ -0,0 +1,9 @@
+function Landing (){
+    return (
+        <>
+            <h1>Lets play!</h1>
+        </>
+    )
+}
+
+export default Landing;
---
+++ client/src/redux/actions.js
@@ -0,0 +1,5 @@
+export function getAllVideogames (){
+    return dispatch => {
+        
+    }
+}
